ef1ecb4445f0080132200272835002b536c899eb
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2025-07-26 21:11:26 +0530

    Update Linked List - Insertion at k-th position.java

--- LINKEDLIST/Linked List - Insertion at k-th position.java
+++ LINKEDLIST/Linked List - Insertion at k-th position.java
@@ -1,22 +1,23 @@
 void insertAfterK(int value, int k) {
     Node newNode = new Node(value);
-    Node current = head;
+    Node temp = head;
     
     // If there are no nodes in the linked list
     // Set the new node as head and return
-    if (current == null) {
+    if (temp == null) {
         head = newNode;
         return;
     }
     
     // Iterate to the k-th node
+    // i want to stop just before the  position where i want to add element
     for (int i = 1; i < k; i++) {
-        current = current.next;
+        temp = temp.next;
     }
-    
-    // Set the next of new Node to next of current
-      newNode.next=current.next;
+    // remember seq is important 1 then 2 
+    // Set the next of new Node to next of current 1.
+      newNode.next=temp.next;
 
-    // Set the next of current to new Node
-    current.next=newNode;
+    // Set the next of current to new Node 2.
+    temp.next=newNode;
 }
